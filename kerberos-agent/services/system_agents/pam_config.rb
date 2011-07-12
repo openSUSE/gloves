db06ea94b3c0b8b4a88135697f0c495a5ab4f260
Josef Reidinger <jreidinger@suse.cz>
2011-07-12 18:39:25 +0200

    restructure little and add Rakefile

---
+++ kerberos-agent/services/system_agents/pam_config.rb
@@ -0,0 +1,24 @@
+$:.unshift(File::join("..", "dbus-infrastructure"))
+
+require 'script_service.rb'
+
+module SystemAgents
+  class PamConfig < ScriptService
+
+    # identification of relevant DBUS service
+    filename = "pam-config"
+
+    def self.execute(params)
+
+      exec_params	= params["exec_params"] || ""
+
+      pam_krb5    = `pam-config #{exec_params}`
+
+      ret	= {
+        "stdout"	=> pam_krb5
+      }
+      return ret
+    end
+
+  end
+end
