fcbf93503ed9c4399cfc617682f7a28f04120163
Jiri Suchomel <jsuchome@suse.cz>
2011-07-12 17:50:43 +0200

    public API for Kerberos client

---
+++ kerberos-client-api/KerberosClientAPI.rb
@@ -0,0 +1,33 @@
+$:.unshift(File::join("..", "kerberos-agent"))
+
+require 'KrbConf.rb'
+require 'PamConfig.rb'
+
+# module for kerberos-client configuration
+module KerberosClientAPI
+
+  def self.read(params)
+    krb5_conf	= KrbConf.read({})
+
+    pam_krb5	= PamConfig.execute({ "exec_params" => "-q --krb5" })["stdout"] || ""
+    sssd	= PamConfig.execute({ "exec_params" => "-q --sss" })["stdout"] || ""
+
+    return {
+	"pam_login"		=> {
+	    "use_kerberos"	=> !pam_krb5.empty?,
+	    "sssd"		=> !sssd.empty?,
+	},
+	"kerberos_client"	=> krb5_conf
+    }
+  end
+
+  def self.write(params)
+    ret = {}
+    return ret
+  end
+
+  def self.propose(params)
+    ret = {}
+    return ret
+  end
+end
