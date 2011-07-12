839461b2232dffe4844e41b4af510ac2e48d34d9
Josef Reidinger <jreidinger@suse.cz>
2011-07-12 18:01:48 +0200

    add script service ancestor

---
+++ dbus-infrastructure/script_service.rb
@@ -0,0 +1,26 @@
+require "dbus_service"
+
+class ScriptService < DbusService
+  FILE_INTERFACE = "org.opensuse.systemagents.script.interface"
+  dbus_interface(FILE_INTERFACE) do
+    dbus_method :read, "out result:v{sv}, in params:v{sv}" do |result,params,user|
+      #TODO permissions check
+      [execute(params)]
+      #TODO exception rescue
+    end
+  end
+
+  def self.service_name
+    "org.opensuse.systemagents.script.#{filename}.service" #TODO check filename characters
+  end
+
+  def self.object_path
+    "org/opensuse/systemagents/script/#{filename}" #TODO check filename characters
+  end
+
+  def self.filename(value=nil)
+    instance_eval "def filename_for_service \"#{value}\" end" if value #FIXME escape VALUE!!
+    raise "File service doesn't define value its file name" unless respond_to? :filename_for_service
+    filename_for_service
+  end
+end
