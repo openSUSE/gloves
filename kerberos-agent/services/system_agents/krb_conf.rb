d3f364113dfb24a78af0dd7c5982f0b422744537
Jiri Suchomel <jsuchome@suse.cz>
2011-07-13 12:31:46 +0200

    read more data from krb5.conf

--- kerberos-agent/services/system_agents/krb_conf.rb
+++ kerberos-agent/services/system_agents/krb_conf.rb
@@ -21,30 +21,42 @@ module SystemAgents
       default_realm	= aug.get("/files/etc/krb5.conf/libdefaults/default_realm")
       default_realm	= "" if default_realm.nil?
 
+      krb5_conf	= {
+	"default_realm"	=> default_realm,
+      }
+
+      clockskew	= aug.get("/files/etc/krb5.conf/libdefaults/clockskew")
+      krb5_conf["clockskew"]	= clockskew unless clockskew.nil?
+
       realms = aug.match("/files/etc/krb5.conf/realms/realm[*]")
     
       puts "found realms: #{realms.inspect}"
 
+      # read data from relevant realm section
       realms.each do |realm_path|
 	realm	= aug.get(realm_path)
 	if (!realm.nil? && realm == default_realm)
-	    kdc			= aug.get(realm_path + "/kdc")
-	    default_domain	= aug.get(realm_path + "/default_domain")
+	    # put current realm data into return map
+	    aug.match(realm_path + "/*").each do |realm_key_path|
+		key	= realm_key_path.split("/").last
+		next if key.index("#comment") == 0
+		krb5_conf[key]	= aug.get(realm_key_path)
+	    end
 	    break
 	end
       end
 
-      kdc		= "" if kdc.nil?
-      default_domain	= "" if default_domain.nil?
-
-      kerberos	= {
-	"kdc"		=> kdc,
-	"default_realm"	=> default_realm,
-	"default_domain"=> default_domain,
-      }
+      # read data from appdefaults/pam section (called 'application' by augeas)
+      unless aug.get("/files/etc/krb5.conf/appdefaults/application").nil?
+	aug.match("/files/etc/krb5.conf/appdefaults/application/*").each do |pam_path|
+	    key	= pam_path.split("/").last
+	    next if key.index("#comment") == 0
+	    krb5_conf[key]	= aug.get(pam_path)
+	end
+      end
 
       aug.close
-      return kerberos
+      return krb5_conf
     end
 
     def write(params)
@@ -58,7 +70,7 @@ module SystemAgents
 
       aug.set("/files/etc/krb5.conf/libdefaults/default_realm", default_realm) unless default_realm.nil?
 
-      # update realm section
+      # update existing realm section
       save_path		= ""
 
       realms		= aug.match("/files/etc/krb5.conf/realms/realm[*]")
@@ -71,7 +83,7 @@ module SystemAgents
       end
       puts "----------- save path: #{save_path}"
 
-      # create new section
+      # ... or create new realm section
       if save_path.empty?
 	save_path	= "/files/etc/krb5.conf/realms/realm[#{realms.size + 1}]"
 	aug.set(save_path, default_realm)
@@ -80,6 +92,8 @@ module SystemAgents
       aug.set(save_path + "/kdc", kdc)
       aug.set(save_path + "/admin_server", kdc)
       aug.set(save_path + "/default_domain", default_domain)
+
+      # FIXME write domain_realm, libdefaults and appdefaults sections
 
       ret	= {
 	"success"	=> true
