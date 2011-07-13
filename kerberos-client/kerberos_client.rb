615298622f33af2b445176261096149f6369e678
Jiri Suchomel <jsuchome@suse.cz>
2011-07-13 17:28:39 +0200

    read ignore_unknown_principals

--- kerberos-client/kerberos_client.rb
+++ kerberos-client/kerberos_client.rb
@@ -11,6 +11,15 @@ module KerberosClient
 
     pam_krb5	= PamConfig.execute({ "exec_params" => "-q --krb5" })["stdout"] || ""
     sssd	= PamConfig.execute({ "exec_params" => "-q --sss" })["stdout"] || ""
+    unless pam_krb5.empty?
+      ignore_unknown	= false
+      pam_krb5.split("\n").each do |line|
+	if line.index("account") == 0
+	    ignore_unknown	= !line.index("ignore_unknown_principals").nil?
+	end
+      end
+      krb5_conf["ignore_unknown"] = ignore_unknown
+    end
 
     # returning same structure as Kerberos::Export
     return {
