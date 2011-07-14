ac3319ce97f7100b2af4d37d2270e3cce7da04ed
Josef Reidinger <jreidinger@suse.cz>
2011-07-14 18:24:03 +0200

    fix test, so bug is not appeared yet :(

--- testC/main.c
+++ testC/main.c
@@ -3,10 +3,11 @@
 int main(int a, char**v)
 {
   ruby_init();
-  ruby_script("yast");
+  ruby_script("ruby");
   ruby_init_loadpath();
-  rb_eval_string("require(\"system_agent/krb5_conf\")");
+  rb_require("system_agent/krb5_conf");
   VALUE module = rb_funcall(rb_funcall( rb_mKernel, rb_intern("const_get"), 1, rb_str_new2("SystemAgent")),rb_intern("const_get"),1,rb_str_new2("Krb5Conf"));
-  rb_funcall2( module,rb_intern("read"),0,0);
+  rb_funcall2( module,rb_intern("read"),1,rb_cObject);
+  rb_eval_string("SystemAgent::Krb5Conf.read({})");
   return 0;
 }
