79487c6f4f697475bc43b334d736c3e29489050d
Jiri Suchomel <jsuchome@suse.cz>
2011-07-29 15:44:18 +0200

    write pam stuff

--- kerberos-client/kerberos_client.rb
+++ kerberos-client/kerberos_client.rb
@@ -6,11 +6,25 @@ require 'system_agent/pam_config'
 # module for kerberos-client configuration
 module KerberosClient
 
+  def self.pam_query(mod)
+    return SystemAgent::PamConfig.execute({ "exec_params" => "-q --" + mod })["stdout"] || ""
+  end
+
+  def self.pam_add(mod)
+    return SystemAgent::PamConfig.execute({ "exec_params" => "-a --" + mod })
+  end
+
+  def self.pam_delete(mod)
+    return SystemAgent::PamConfig.execute({ "exec_params" => "-d --" + mod })
+  end
+
   def self.read(params)
     krb5_conf	= SystemAgent::Krb5Conf.read({})
+    # FIXME read /etc/ssh/ssh_config
 
-    pam_krb5	= SystemAgent::PamConfig.execute({ "exec_params" => "-q --krb5" })["stdout"] || ""
-    sssd	= SystemAgent::PamConfig.execute({ "exec_params" => "-q --sss" })["stdout"] || ""
+    pam_krb5	= pam_query("krb5")
+    sssd	= pam_query("sss")
+
     unless pam_krb5.empty?
       ignore_unknown	= false
       pam_krb5.split("\n").each do |line|
@@ -32,9 +46,38 @@ module KerberosClient
   end
 
   def self.write(params)
+
+    # save config file settings
     krb5_conf	= params["kerberos_client"]
     krb5_conf	= {} if krb5_conf.nil?
     ret = SystemAgent::Krb5Conf.write(krb5_conf)
+
+    return ret unless ret["success"] 
+
+    # update PAM configuration
+    sssd	= params["pam_login"]["sssd"]
+
+    # sssd was set up (from different place)
+    if sssd
+      # FIXME update sssd.conf file here
+      pam_delete("krb5")
+    # standard authentication (krb5) is on
+    elsif params["pam_login"]["use_kerberos"]
+      # combined LDAP+Kerberos setup
+      if pam_query("ldap").empty?
+	pam_add("krb5")
+      else
+	pam_add("ldap-account_only")
+      end
+      pam_add("krb5-ignore_unknown_principals") if params["kerberos_client"]["ignore_unknown"]
+    # standard authentication (krb5) is off
+    else
+      pam_delete("krb5")
+      # Kerberos removed from combined LDAP+Kerberos setup
+      pam_add("ldap") if pam_query("ldap-account_only")
+
+    end
+
     return ret
   end
 
