d2d1dd2bccbf2f730d3a8205ec0aaff899eb340e
Josef Reidinger <jreidinger@suse.cz>
2011-08-11 14:16:16 +0200

    redirect output of dbus service to log

--- systemagents/bin/file_services/krb5_conf_service.rb
+++ systemagents/bin/file_services/krb5_conf_service.rb
@@ -1,4 +1,6 @@
 #!/usr/bin/env ruby
+$stdout.reopen("/var/log/systemagent.stdout")
+$stderr.reopen("/var/log/systemagent.stderr")
 $LOAD_PATH << File.join(File.dirname(__FILE__),'..','..',"services")
 require "system_agents/krb5_conf"
 require "dbus_services/service_runner"
