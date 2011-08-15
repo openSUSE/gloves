cf29886aa9dcc795cd6b728f92352d8961e5d804
Jiri Suchomel <jsuchome@suse.cz>
2011-08-15 15:30:19 +0200

    read pkinit section 1st writing example

--- systemagents/test/test_krb5_conf.rb
+++ systemagents/test/test_krb5_conf.rb
@@ -6,12 +6,27 @@ require "system_agents/krb5_conf"
 class TestKrb5Conf < Test::Unit::TestCase
   def setup
     @data_dir = File.join(File.dirname(__FILE__),"data")
+    @data1_dir = File.join(File.dirname(__FILE__),"data1")
   end
 
   def test_reading
     file = SystemAgents::Krb5Conf.new nil
     ret = file.read "_aug_internal" => Augeas::open(@data_dir,nil, Augeas::NO_MODL_AUTOLOAD)
     assert_equal "example.cz", ret["default_domain"]
+    assert_equal "kdc.example.cz", ret["kdc"]
+    assert_equal "1d", ret["ticket_lifetime"]
+    assert_equal "ad.example.cz", ret["trusted_servers"]
+  end
+
+  def test_write
+    file = SystemAgents::Krb5Conf.new nil
+    params	= {
+	"_aug_internal" => Augeas::open(@data1_dir,nil, Augeas::NO_MODL_AUTOLOAD),
+	"default_domain"	=> "example.de",
+	"default_realm"		=> "EXAMPLE.DE"
+    }
+    ret = file.write params
+    assert_equal true, ret["success"]
   end
 end
 
