50a0afab6de59df91ed77d8ef2994fbbe0e0e958
Jiri Suchomel <jsuchome@suse.cz>
2011-08-23 16:06:35 +0200

    testing read

--- systemagents/test/test_ssh_config.rb
+++ systemagents/test/test_ssh_config.rb
@@ -7,6 +7,16 @@ class TestSshConfig < Test::Unit::TestCase
   def setup
     @data_dir = File.join(File.dirname(__FILE__),"data")
   end
+
+  def test_reading
+    file = SystemAgents::SshConfig.new nil
+    ret = file.read "_aug_internal" => Augeas::open(@data_dir,"/tmp/lens", Augeas::NO_MODL_AUTOLOAD)
+    assert_equal ["LC_IDENTIFICATION", "LC_ALL"], ret["SendEnv"]
+    assert_equal "suse.cz", ret["Host"][0]["Host"]
+    assert_equal "*", ret["Host"][1]["Host"]
+    assert_equal ["LC_LANG"], ret["Host"][1]["SendEnv"]
+  end
+
 end
 
 Test::Unit::UI::Console::TestRunner.run(TestSshConfig)
