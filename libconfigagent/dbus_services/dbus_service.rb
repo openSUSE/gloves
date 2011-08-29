ea797f2cbb1afdaac0c6511c9b901bd9aca52121
Josef Reidinger <jreidinger@suse.cz>
2011-08-29 13:59:03 +0200

    fix license, as we use LGPL not GPL and uniform header with other project

--- libconfigagent/dbus_services/dbus_service.rb
+++ libconfigagent/dbus_services/dbus_service.rb
@@ -1,19 +1,20 @@
-=begin
-This file is part of LibConfigAgent.
-
-LibConfigAgent is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-version 2.1 of the License.
-
-LibConfigAgent is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with LibConfigAgent.  If not, see <http://www.gnu.org/licenses/>.
-=end
+#--
+# Config Agents Framework
+#
+# Copyright (C) 2011 Novell, Inc. 
+#   This library is free software; you can redistribute it and/or modify
+# it only under the terms of version 2.1 or version 3 of the GNU Lesser General Public
+# License as published by the Free Software Foundation. 
+#
+#   This library is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more 
+# details. 
+#
+#   You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software 
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#++
 
 require "rubygems"
 require "dbus"
