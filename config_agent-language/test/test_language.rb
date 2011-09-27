09f6695ba0e8a09db0f575bc685e73e717b0c1fe
Jiri Suchomel <jsuchome@suse.cz>
2011-09-27 09:28:15 +0200

    new agent for /etc/sysconfig/language

---
+++ config_agent-language/test/test_language.rb
@@ -0,0 +1,38 @@
+#--
+# Config Agents Framework
+#
+# Copyright (C) 2011 Novell, Inc. 
+#   This library is free software; you can redistribute it and/or modify
+# it only under the terms of version 2.1 or version 3 of the GNU Lesser General Public
+# License as published by the Free Software Foundation. 
+#
+#   This library is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more 
+# details. 
+#
+#   You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software 
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#++
+
+$LOAD_PATH.unshift File.join(File.dirname(__FILE__),'..','services')
+require "test/unit/testcase"
+require 'test/unit/ui/console/testrunner'
+require "rubygems"
+require "config_agent_service/language"
+
+class TestLanguage < Test::Unit::TestCase
+  def setup
+    @data_dir = File.join(File.dirname(__FILE__),"data")
+  end
+
+  def test_reading
+    file = ConfigAgentService::Language.new nil
+    sysconfig_language = file.read "_aug_internal" => Augeas::open(@data_dir, File.join(File.dirname(__FILE__),'..',"lens"),Augeas::NO_MODL_AUTOLOAD)
+    assert_equal "cs_CZ.UTF-8", sysconfig_language["RC_LANG"]
+    assert_equal "", sysconfig_language["INSTALLED_LANGUAGES"]
+  end
+end
+
+Test::Unit::UI::Console::TestRunner.run(TestLanguage)
