3caf0df71981001e7200dfad171e5631454f91ca
Josef Reidinger <jreidinger@suse.cz>
2011-12-15 12:09:20 +0100

    fixes in windowmanager

--- config_agent-windowmanager/services/file/windowmanager.rb
+++ config_agent-windowmanager/services/file/windowmanager.rb
@@ -17,6 +17,7 @@
 #++
 
 require 'config_agent_service/file_service'
+require 'augeas'
 
 class Windowmanager < ConfigAgentService::FileService
 
@@ -32,6 +33,8 @@ class Windowmanager < ConfigAgentService::FileService
       "default_wm" => default_wm
     }
     aug.close
+
+    log.info windowmanager_conf.inspect
 
     return windowmanager_conf
   end
