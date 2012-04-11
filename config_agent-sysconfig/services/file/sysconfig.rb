ab42eda494ba36d99e5c4f5a8c74e3a3def117df
Michal Filka <mfilka@suse.cz>
2012-04-11 13:18:40 +0200

    initial import for sysconfig file agent and related examples

---
+++ config_agent-sysconfig/services/file/sysconfig.rb
@@ -0,0 +1,92 @@
+#--
+# Config Agents Framework
+#
+# Copyright (C) 2011 Novell, Inc.
+#   This library is free software; you can redistribute it and/or modify
+# it only under the terms of version 2.1 or version 3 of the GNU Lesser General Public
+# License as published by the Free Software Foundation.
+#
+#   This library is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+# details.
+#
+#   You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#++
+
+require 'config_agent_service/file_service' # Gloves only
+require 'augeas'
+
+# TODO: read, write: params[ "file"] should contain absolute path - add check
+class Sysconfig < ConfigAgentService::FileService
+#class Sysconfig    # YaST2
+
+    SYSCONFIG_LENS = "Shellvars.lns"
+
+    def read(params)
+        return {} if !params.has_key?( "file");
+
+        file = params[ "file"];
+        ret = {}
+
+        aug = load_augeas(params)
+
+        unless aug.get("/augeas/files#{file}/error").nil?
+            aug.close
+            return ret
+        end
+
+        aug.match("/files#{file}/*").each do |key_path|
+            key = key_path.split("/").last
+# do not ignore comments, there are several bugs on YaST2 (e.g. comments got lost, ...)
+# TODO: configurable option?
+#      next if key.start_with? "#comment"
+
+            # remove quotes from value (Shellvars.lns keeps quoting)
+            ret[key] = aug.get(key_path)[/[^'"].*[^'"]/]
+        end
+
+        aug.close
+
+        return ret
+    end
+
+    def write(params)
+        return { "success" => false, "message" => "Missing desc file" } if !params.has_key?( "file");
+
+        file = params[ "file"];
+        ret = {
+          "success" => true
+        }
+
+        aug = load_augeas(params)
+
+        params.each do |key, value|
+            next if key.start_with? "_"   # skip internal keys
+            next if key == "file"         # destination flag.
+            aug.set("/files#{file}/#{key}", value)
+        end
+
+        unless aug.save
+            ret["success"] = false
+            ret["message"] = aug.get("/augeas/files#{file}/error/message")
+        end
+
+        aug.close
+        
+        return ret
+    end
+
+  private
+
+  def load_augeas(params)
+    aug = params["_aug_internal"] || Augeas::open(nil, "", Augeas::NO_MODL_AUTOLOAD)
+    aug.transform(:lens => SYSCONFIG_LENS, :incl => params[ "file"])
+    aug.load
+
+    return aug
+  end
+
+end
---
+++ yast++lib-sysconfig/lib/y_lib/sysconfig.rb
@@ -0,0 +1,57 @@
+#--
+# YaST++ Users Library
+#
+# Copyright (C) 2011 Novell, Inc.
+#   This library is free software; you can redistribute it and/or modify
+# it only under the terms of version 2.1 or version 3 of the GNU Lesser General Public
+# License as published by the Free Software Foundation.
+#
+#   This library is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+# details.
+#
+#   You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#++
+
+$LOAD_PATH << File.dirname(__FILE__)
+
+require 'config_agent/sysconfig'
+
+# module for users configuration
+module YLib
+
+  module Sysconfig
+
+    def self.last_error
+      return @error
+    end
+
+    # Read all settings relevant for Users configuration
+    # Standard Gloves way
+    def self.read(params)
+      ret       = {}
+
+      begin
+        ret     = ConfigAgent::Sysconfig.read(params)
+      rescue DbusClients::InsufficientPermission => e
+        @error	= "User has no permission for action '#{e.permission}'."
+        return nil
+      end
+      return ret;
+    end
+
+    def self.write( params)
+        ret = {}
+
+        ret = ConfigAgent::Sysconfig.write( params)
+
+        return ret
+    end
+
+private
+
+  end
+end
