4dc30f9c6036d5944f7a7f486e92a9fcb62c2f95
Jiri Suchomel <jsuchome@suse.cz>
2012-06-04 14:15:05 +0200

    agent for /etc/adjtime (for HW clock)

---
+++ config_agent-country/lib/config_agent/adjtime.rb
@@ -0,0 +1,77 @@
+#--
+# Config Agents Framework
+#
+# Copyright (C) 2011 Novell, Inc.
+#   This library is free software; you can redistribute it and/or modify
+# it only under the terms of version 2.1 or version 3 of the GNU Lesser General Public
+# License as published by the Free Software Foundation.
+#
+#   This library is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+# details.
+#
+#   You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#++
+
+require 'config_agent/file_agent'
+require "augeas"
+
+module ConfigAgent
+  class Adjtime < ConfigAgent::FileAgent
+
+    FILE_PATH = "/etc/adjtime"
+
+    def read(params)
+
+      aug = load_augeas(params)
+
+      ret = {} 
+
+      unless aug.get("/augeas/files#{FILE_PATH}/error").nil?
+        aug.close
+        return ret
+      end
+
+      aug.match("/files#{FILE_PATH}/*").each do |key_path|
+        key = key_path.split("/").last
+        ret[key] = aug.get(key_path)
+      end
+
+      aug.close
+      return ret
+    end
+
+    def write(params)
+      
+      ret = {
+        "success" => true
+      }
+      aug = load_augeas(params)
+
+      params.each do |key, value|
+        aug.set("/files#{FILE_PATH}/#{key.to_i}", value) unless key.to_i == 0
+      end
+
+      unless aug.save
+        ret["success"] = false
+        ret["message"] = aug.get("/augeas/files#{FILE_PATH}/error/message")
+      end
+
+      aug.close
+      return ret
+    end
+
+  private
+    
+    def load_augeas(params)
+      aug = params["_aug_internal"] || Augeas::open(nil, "/usr/share/augeas/lenses/", Augeas::NO_MODL_AUTOLOAD)
+      aug.transform(:lens => "Adjtime.lns", :incl => FILE_PATH)
+      aug.load
+      return aug
+    end
+  
+  end
+end
---
+++ config_agent-country/test/test_adjtime.rb
@@ -0,0 +1,69 @@
+#--
+# Config Agents Framework
+#
+# Copyright (C) 2011 Novell, Inc.
+#   This library is free software; you can redistribute it and/or modify
+# it only under the terms of version 2.1 or version 3 of the GNU Lesser General Public
+# License as published by the Free Software Foundation.
+#
+#   This library is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+# details.
+#
+#   You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#++
+
+$LOAD_PATH.unshift File.join(File.dirname(__FILE__),'..','lib')
+require "test/unit/testcase"
+require 'test/unit/ui/console/testrunner'
+require "rubygems"
+require "config_agent/adjtime"
+
+class TestAdjtime < Test::Unit::TestCase
+  def setup
+    @data_dir = File.join(File.dirname(__FILE__),"data")
+    @data1_dir = File.join(File.dirname(__FILE__),"data1")
+    @data2_dir = File.join(File.dirname(__FILE__),"data2")
+  end
+
+  def test_reading
+    file = ConfigAgent::Adjtime.new
+    adjtime = file.read "_aug_internal" => Augeas::open(@data_dir, File.join(File.dirname(__FILE__),'..',"lens"),Augeas::NO_MODL_AUTOLOAD)
+    assert_equal "LOCAL", adjtime["3"]
+  end
+
+  # write new file
+  def test_write
+    file = ConfigAgent::Adjtime.new
+    params	= {
+      "_aug_internal"   => Augeas::open(@data1_dir,nil, Augeas::NO_MODL_AUTOLOAD),
+      "3" => "UTC",
+      "2" => "0",
+      "1" => "0.0 0 0.0"
+    }
+    ret = file.write params
+    assert_equal true, ret["success"]
+    assert_equal nil, ret["message"]
+  end
+
+  # diff data/etc/adjtime data2/etc/adjtime -> change LOCAL to UTC
+  def test_overwrite
+    file = ConfigAgent::Adjtime.new
+    params = file.read "_aug_internal" => Augeas::open(@data_dir,nil, Augeas::NO_MODL_AUTOLOAD)
+    assert_equal "LOCAL", params["3"]
+
+    file2 = ConfigAgent::Adjtime.new
+    params["_aug_internal"]	= Augeas::open(@data2_dir,nil, Augeas::NO_MODL_AUTOLOAD)
+    params["3"]   = "UTC"
+
+    ret = file2.write params
+    assert_equal nil, ret["message"]
+  end
+
+
+end
+
+Test::Unit::UI::Console::TestRunner.run(TestAdjtime)
